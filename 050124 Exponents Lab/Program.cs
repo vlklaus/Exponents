921393fea9416f51075da7152c92f0446d33ba3d
Victoria Klaus <vlklaus22@gmail.com>
2024-05-01 17:56:21 -0400

    first commit

---
+++ 050124 Exponents Lab/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Learn your squares and cubes!");
+
+
+bool again = true;
+while (again)
+{
+    Console.Write("Enter an integer up to 1290: ");
+    int value = int.Parse(Console.ReadLine());
+
+    //validation
+    if (value < 1)
+    {
+        continue;
+    }
+
+    //header {x,indent:F1}
+    Console.WriteLine("Number\t\tSquared\t\tCubed");
+    Console.WriteLine("=======\t\t=======\t\t======");
+
+    for (int row = 1; row <= value; row++)
+    {
+        int squared = row * row;
+        int cubed = row * row * row;
+
+        Console.WriteLine($"{row,7} {squared,15} {cubed,14}");
+    }
+
+    Console.WriteLine("Continue? (y/n): ");
+    string response = Console.ReadLine();
+
+    if (response == "y")
+    {
+        continue;
+    }
+    else
+    {
+        again = false;
+        break;
+    }
+  
+}
+
+
+
+
